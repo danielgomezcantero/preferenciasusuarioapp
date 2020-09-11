638fb9963b450a7ca068c7e7a32bd9f07dabc267
Daniel Maximiliano Gómez Cantero <danielgomezcantero@gmail.com>
2020-09-11 14:44:56 -0400

    Drawer como widget independiente

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:preferenciasusuarioapp/src/pages/settings_page.dart';
+
+import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = 'home';
@@ -9,7 +10,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias del usuario'),
       ),
-      drawer: _crearManu(context),
+      drawer: MenuWidget(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -19,59 +20,6 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre de usuario:'),
           Divider(),
-        ],
-      ),
-    );
-  }
-
-  Drawer _crearManu(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            child: Container(),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage('assets/menu-img.jpg'),
-              fit: BoxFit.cover,
-            )),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.pages,
-              color: Colors.blue,
-            ),
-            title: Text('Home'),
-            onTap: () =>
-                Navigator.pushReplacementNamed(context, HomePage.routeName),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.party_mode,
-              color: Colors.blue,
-            ),
-            title: Text('Party Mode'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.people,
-              color: Colors.blue,
-            ),
-            title: Text('People'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.settings,
-              color: Colors.blue,
-            ),
-            title: Text('Settings'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
-            },
-          )
         ],
       ),
     );
--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static final String routeName = 'setting';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'Daniel';
+
+  TextEditingController _textController;
+  @override
+  void initState() {
+    super.initState();
+    _textController = new TextEditingController(text: _nombre);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Ajustes'),
         ),
-        body: Center(
-          child: Text('Ajustes page'),
+        drawer: MenuWidget(),
+        body: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                'Settings',
+                style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Divider(),
+            SwitchListTile(
+              value: _colorSecundario,
+              title: Text('Color secundario'),
+              onChanged: (value) {
+                _colorSecundario = value;
+                setState(() {});
+              },
+            ),
+            RadioListTile(
+              value: 1,
+              title: Text('Masculino'),
+              groupValue: _genero,
+              onChanged: (value) {
+                _genero = value;
+                setState(() {});
+              },
+            ),
+            RadioListTile(
+              value: 2,
+              title: Text('Femenino'),
+              groupValue: _genero,
+              onChanged: (value) {
+                _genero = value;
+                setState(() {});
+              },
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                    labelText: 'Nombre',
+                    helperText: 'Nombre que usa el teléfono'),
+                onChanged: (value) {},
+              ),
+            )
+          ],
         ));
   }
 }
